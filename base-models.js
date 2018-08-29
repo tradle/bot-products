945c99d434dea8f0a245099d56f55f90551af0aa
Mark Vayngrib <mark@tradle.io>
2018-08-28 21:25:26 -0400

    refactor: adapt to latest models

--- base-models.js
+++ base-models.js
@@ -1,6 +1,6 @@
 const mergeModels = require('@tradle/merge-models')
 const base = require('@tradle/models').models
-const custom = require('@tradle/custom-models')
+const custom = require('@tradle/custom-models').models
 const productsBot = require('@tradle/models-products-bot')
 
 module.exports = mergeModels()
